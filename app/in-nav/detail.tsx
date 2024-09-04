8de78dbfaa1632b168116928a3d864b6594fe2a2
Jell <mystral7@naver.com>
2024-09-04 21:27:53 +0900

    [Coin] add detail(victory chart)

--- app/in-nav/detail.tsx
+++ app/in-nav/detail.tsx
@@ -1,14 +1,34 @@
 import { info, history } from '@/apis'
 import { Icon } from '@/components/Coin'
+import { BLACK_COLOR } from '@/utils/colors'
 import { useRoute } from '@react-navigation/native'
 import { useNavigation } from 'expo-router'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
+import {
+  CartesianChart,
+  Line,
+  PointsArray,
+  Scatter,
+  useAnimatedPath,
+  useLinePath
+} from 'victory-native'
 
 import styled from 'styled-components/native'
+import { View } from 'react-native'
+import { Path } from '@shopify/react-native-skia'
 
-const Wrapper = styled.View``
-const Text = styled.Text``
+const Container = styled.ScrollView`
+  background-color: ${BLACK_COLOR};
+`
+
+const InterpolationLine = ({ points }: { points: PointsArray }) => {
+  const { path } = useLinePath(points, { curveType: 'monotoneX' })
+  const animPath = useAnimatedPath(path, { type: 'spring' })
+  return (
+    <Path path={animPath} style="stroke" strokeWidth={2} color={'#1abc9c'} />
+  )
+}
 
 const detail = () => {
   const { params }: { params: { symbol: string; id: string } } = useRoute()
@@ -38,13 +58,39 @@ const detail = () => {
     ['coinHistory', params.id],
     history
   )
-
-  console.log('info', infoData)
-  console.log('history', historyData)
+  const [victoryData, setVictoryData] = useState([])
+  useEffect(() => {
+    if (historyData) {
+      setVictoryData(
+        historyData.map((price: { timestamp: string; price: string }) => ({
+          x: new Date(price.timestamp).getTime(),
+          y: price.price
+        }))
+      )
+    }
+  }, [historyData])
+  console.log('victoryData', victoryData)
   return (
-    <Wrapper>
-      <Text>Detail</Text>
-    </Wrapper>
+    <Container>
+      {!historyData ? null : (
+        <View style={{ height: 360 }}>
+          <CartesianChart data={victoryData} xKey="time" yKeys={['y']}>
+            {({ points }) => (
+              <>
+                <InterpolationLine points={points.y} />
+                <Scatter
+                  points={points.y}
+                  shape="circle"
+                  radius={3}
+                  style="fill"
+                  color={'#1abc9c'}
+                />
+              </>
+            )}
+          </CartesianChart>
+        </View>
+      )}
+    </Container>
   )
 }
 export default detail
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin']
     // presets: ['module:metro-react-native-babel-preset'],
 
     // env: {
